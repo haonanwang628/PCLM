34ef6e333edf7a9dadfb6d9406485536b08042a5
Junf <2809959346@qq.com>
2025-11-06 22:20:29 +0800

    junf

--- config/model_menu.py
+++ config/model_menu.py
@@ -7,19 +7,27 @@ MODEL_TOKENIZER_MAP = {
 llm_MaxToken = {
     "deepseek-chat": 8000,
     "deepseek-reasoner": 10000,
-    "gpt-4o-mini": 16384
+    "gpt-4o-mini": 16384,
+    "gpt-4o": 16384,
+    "gpt-5": 16384
 }
 
-available_models = ["deepseek-chat", "gpt-4o-mini", "Claude"]
+available_models = ["deepseek-chat", "gpt-4o-mini", "gpt-4o", "gpt-5"]
 
+
+import os
 api_key = {
-    "gpt-4o-mini": "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",
-    "deepseek-chat": "sk-9bc06d1289704b05b7b52db5285dba67",
-    "deepseek-reasoner": "sk-9bc06d1289704b05b7b52db5285dba67"
+    "gpt-4o": os.getenv("DEEPSEEK_API_KEY"),
+    "gpt-4o-mini": os.getenv("DEEPSEEK_API_KEY"),
+    "deepseek-chat": os.getenv("DEEPSEEK_API_KEY"),
+    "deepseek-reasoner": os.getenv("DEEPSEEK_API_KEY"),
+    "gpt-5": os.getenv("DEEPSEEK_API_KEY")
 }
 
 base_url = {
     "gpt-4o-mini": "",
+    "gpt-4o": "",
+    "gpt-5": "",
     "deepseek-chat": "https://api.deepseek.com/v1",
     "deepseek-reasoner": "https://api.deepseek.com/v1",
 }
